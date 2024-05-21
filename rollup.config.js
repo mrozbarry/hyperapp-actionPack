2f7ca7537d34b6b3b45f4b1c1ee748ccb1fbe368
Alex Barry <alex.barry@gmail.com>
2024-05-21 09:51:07 -0400

    Fix esm browser export

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'actionPack.js',
   output: {
-    file: 'actionPack.browser.js',
-    format: 'amd'
+    file: 'actionPack.bundle.js',
+    format: 'esm'
   },
-  plugins: [nodeResolve()]
+  plugins: [
+    commonjs(),
+    nodeResolve({
+      preferBuiltins: true,
+    }),
+  ]
 };
